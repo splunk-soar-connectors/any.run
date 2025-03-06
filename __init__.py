8d42d3b7e92b9319417d97fa1945337dfe1f6b36
mmomin-crest <mmomin@splunk.com>
2025-03-06 14:33:21 +0530

    Updated the copyright year

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 # File: __init__.py
 #
-# Copyright (c) ANYRUN FZCO, 2024
+# Copyright (c) ANYRUN FZCO, 2025
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
